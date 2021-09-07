3c7c4b251fa577beabaae8564cdc4a69a2ce5ca6
T Nandan Pai <pai.nandan2@gmail.com>
2021-09-07 17:06:13 +0530

    Adding the files to Github.

--- src/main/java/com/pluralsight/service/RideService.java
+++ src/main/java/com/pluralsight/service/RideService.java
@@ -5,7 +5,16 @@ import java.util.List;
 import com.pluralsight.model.Ride;
 
 public interface RideService {
+	
+	Ride createRide(Ride ride);
 
 	List<Ride> getRides();
 
+	Ride getRide(Integer id);
+
+	Ride updateRide(Ride ride);
+
+	void batch();
+
+	void deleteRide(Integer id);
 }
--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -4,21 +4,35 @@ import java.util.List;
 
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.RestTemplate;
 
 import com.pluralsight.model.Ride;
+import com.pluralsight.util.ServiceError;
 
 import org.junit.Test;
 
 public class RestControllerTest {
 
 	@Test(timeout=3000)
+	public void testCreateRide() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		Ride ride = new Ride();
+		ride.setName("Cannonball Trail");
+		ride.setDuration(69);
+		
+		ride = restTemplate.postForObject("http://localhost:8081/ride_tracker/ride", ride, Ride.class);
+	}
+	
+	@Test(timeout=3000)
 	public void testGetRides() {
 		RestTemplate restTemplate = new RestTemplate();
 
 		ResponseEntity<List<Ride>> ridesResponse = restTemplate.exchange(
-				"http://localhost:8080/ride_tracker/rides", HttpMethod.GET,
+				"http://localhost:8081/ride_tracker/rides", HttpMethod.GET,
 				null, new ParameterizedTypeReference<List<Ride>>() {
 				});
 		List<Ride> rides = ridesResponse.getBody();
@@ -26,5 +40,53 @@ public class RestControllerTest {
 		for (Ride ride : rides) {
 			System.out.println("Ride name: " + ride.getName());
 		}
+	}
+	
+	@Test(timeout=3000)
+	public void testGetRide() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		Ride ride = restTemplate.getForObject("http://localhost:8081/ride_tracker/ride/1", Ride.class);
+		
+		System.out.println("Ride name: " + ride.getName());
+		
+	}
+	
+	@Test(timeout=3000)
+	public void testUpdateRide() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		Ride ride = restTemplate.getForObject("http://localhost:8081/ride_tracker/ride/1", Ride.class);
+
+		ride.setDuration(ride.getDuration() + 1);
+		
+		restTemplate.put("http://localhost:8081/ride_tracker/ride", ride);
+		
+		System.out.println("Ride name: " + ride.getName());
+		
+	}
+	
+	@Test(timeout=3000)
+	public void testBatchUpdate() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		restTemplate.getForObject("http://localhost:8081/ride_tracker/batch", Object.class);
+		
+	}
+	
+	@Test(timeout=3000)
+	public void testDelete() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		restTemplate.delete("http://localhost:8081/ride_tracker/delete/9");
+		
+	}
+	
+	@Test(timeout=3000)
+	public void testException() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		restTemplate.getForObject("http://localhost:8081/ride_tracker/test", Ride.class);
+		
 	}
 }
